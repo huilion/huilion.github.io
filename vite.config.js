5633d66772ba9c96b79a279e30d097317c61a0aa
huilin <ejk2827@g.rit.edu>
2025-06-06 19:06:36 -0500

    again...

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./huilion.github.io",
+  base: "/huilion.github.io",
   plugins: [react()],
 })
