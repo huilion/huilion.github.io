94ca8d29abc6d1f172a52ec3b96efde694f6414b
huilin <ejk2827@g.rit.edu>
2025-06-09 15:42:33 -0500

    Update resume location

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
                 <p>
                     My name is Huilin Khouw and I am a fourth year Game Design and Development major at the Rochester Institute of Technology.
                     I also am pursuing a minor in Web Development. I am aiming for a career focused on design and frontend development. For more information
-                    on my academic and professional career, <a href="src\assets\EJKhouwResumer.pdf" download="HKhouwResume">click here</a> to download my resume. 
+                    on my academic and professional career, <a href="https://github.com/huilion/huilion.github.io/blob/main/src/assets/EJKhouwResumer.pdf" download="HKhouwResume">click here</a> to download my resume. 
                 </p>
             </div>
         </div>
